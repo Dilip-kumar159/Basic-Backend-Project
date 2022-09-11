587dca7f86b096ceafc2851cee3ad8e6e055e8df
Dilip <dilip45377@gmail.com>
2022-09-11 23:34:46 +0530

    add ejs

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+// setting up the view engine
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){

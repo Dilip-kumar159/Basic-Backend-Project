a0de077249e13b998a844432cec18fb806331f7d
Dilip <dilip45377@gmail.com>
2022-09-11 23:42:45 +0530

    add views for home ejs

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,9 +1,6 @@
 module.exports.home = function(req, res){
-    console.log("This is running in the browser");
-     return res.end('<h1> Express is up for Codeial </h1>');
-}
-
-module.exports.name = function(req, res){
-    console.log("This is not running in the browser");
-    return  res.end('<h1> This is created by Dilip </h1>');
+    
+    return res.render('home', {
+        title : "Home"
+    });
 }

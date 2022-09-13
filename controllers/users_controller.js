b5f20616ffeb811e6d13b59e4aeb7b3364b894e9
Dilip <dilip45377@gmail.com>
2022-09-13 14:07:36 +0530

    add user sign-up and sign-in page in Views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,31 @@ module.exports.profile = function(req, res){
         title : "User Profile"
     })
 }
+
+// Rendring The Sign Up page
+module.exports.signUp = function(req, res){
+
+    return res.render('user_sign_up', {
+        title : "Codeial | Sign Up"
+    })
+}
+
+// Rendring the Sign In page
+module.exports.signIn = function(req, res){
+
+    return res.render('user_sign_in', {
+        title : "Codeial | Sign In"
+    })
+}
+
+// Get The Sign-Up data
+module.exports.create = function(req, res){
+    // TODO later
+
+}
+
+// Sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    // TODO later
+
+}
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,7 @@ const users_controllers = require('../controllers/users_controller');
 
 router.get('/profile', users_controllers.profile);
 
+router.get('/Sign-up', users_controllers.signUp);
+router.get('/Sign-In', users_controllers.signIn);
+
 module.exports = router;

fb8bd6e213c9c46bfd639ba7d68fd3d022692826
Dilip <dilip45377@gmail.com>
2022-09-24 19:49:48 +0530

    Added sass styling

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ const passportLocal = require('./config/passport-local-strategy');
 
 const mongoSessionStore = require("connect-mongo");
 const MongoStore = mongoSessionStore(session);
+const sassMiddleware = require('node-sass-middleware');
 
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 
 app.use(express.urlencoded());
 

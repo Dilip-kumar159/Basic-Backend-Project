b60d7b8f12f38d8a227b1b689a6c5b4bbbfa6016
Dilip <dilip45377@gmail.com>
2022-10-02 19:16:15 +0530

    created scss file and use async in the code

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,25 +1,36 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
 
-    // populate the user of each post
-    Post.find({})
+    try{
+         // populate the user of each post
+    let posts = await Post.find({})
     .populate('user')
     .populate({
         path: 'comments',
         populate: {
             path: 'user'
         }
-    })
-    .exec(function(err, posts){
+    });
+    
+    let users = await User.find({});
 
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title : "Codieal | Home", 
-                posts: posts, 
-                all_users: users
-            });
-        }); 
-    });  
+        return res.render('home', {
+            title : "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+
+    }catch(err){
+        console.log("Error ", err);
+        return;
+    } 
 }
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then();
--- models/post.js
+++ models/post.js
@@ -10,12 +10,15 @@ const postSchema = new mongoose.Schema({
         ref : 'User'
     },
     // include the array of ids of all comments in this post schema itself
-    comments : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref : 'Comment'
-    }
-},
-    { timestamps : true});
+    comments : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : 'Comment'
+        }
+    ]
+}, { 
+    timestamps : true
+});
 
 
 const Post = mongoose.model('Post', postSchema);

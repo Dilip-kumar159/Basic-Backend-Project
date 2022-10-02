46560c838e9e2b92967d13f803b51285b2f0a8b4
Dilip <dilip45377@gmail.com>
2022-10-02 13:15:18 +0530

    created posts_controller and posts_route

---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const postsController = require('../controllers/posts_controllers');
+
+router.post('/create', postsController.create);
+
+module.exports = router;

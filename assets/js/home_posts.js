d6b566e7543858021f8faa05a9d8844233f81907
Dilip <dilip45377@gmail.com>
2022-10-09 17:11:48 +0530

    modified controllers

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 }, error: function(error){
                     console.log(error.responseTEXt);
                 }
@@ -27,7 +28,7 @@
         <p>
         
          <small>
-             <a class="delete-post-button" href="/posts/destroy/${post.id}"> X </a>
+             <a class="delete-post-button" href="/posts/destroy/${post._id}"> X </a>
          </small>
         
          ${post.content}
@@ -52,6 +53,25 @@
          </div>
          </div>
      </li>`)
+    }
+
+
+    // Method to delete the post from DOM
+
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink.prop('href')),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        });
     }
     createPost();
 }
--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -35,6 +35,16 @@ module.exports.destroy = async function(req, res){
 
         await Comment.deleteMany({post: req.params.id});
 
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post_id: req.params.id
+                },
+                message: "Post Deleted"
+            })
+        }
+        
         req.flash('success', 'Post and associated comments deleted');
 
              return res.redirect('back');

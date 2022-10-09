14f7ddae71a2a838611082a0406b28707a8a9d5d
Dilip <dilip45377@gmail.com>
2022-10-09 21:02:38 +0530

    modified jwt Stratergy

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -26,7 +26,8 @@ module.exports.destroy = async function(req, res){
     try{
         let post = await Post.findById(req.params.id);
 
-    // if(post.user == req.user.id){
+    if(post.user == req.user.id){
+        
         post.remove();
 
         await Comment.deleteMany({post: req.params.id});
@@ -34,11 +35,11 @@ module.exports.destroy = async function(req, res){
              return res.json(200, {
                 message: 'Post and associated comments deleted succesfully!!'
              });
-    // }else{
-    //     req.flash('error', 'You can delete this post');
-
-    //     return res.redirect('back');
-    //     }
+    }else{
+            return res.json(401,{
+                message: 'You cannot delete this post'
+            });
+        }
     }catch(err){
         console.log('*****', err);
         return res.json(500, {
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -99,7 +99,6 @@ module.exports.create = function(req, res){
 module.exports.createSession = function(req, res){
     req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
-
 }
 
 // For signing out 

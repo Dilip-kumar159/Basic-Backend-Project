cea70f1ee27b024f6afd17193e32c32af512366f
Dilip <dilip45377@gmail.com>
2022-10-09 19:34:34 +0530

    userDefine api created

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+
 const homeController = require('../controllers/home_controllers');
 console.log("Router Loaded in route folder");
 
@@ -13,4 +14,7 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 
 router.use('/comments', require('./comments'));
+
+router.use('/api', require('./api'));
+
 module.exports = router;
